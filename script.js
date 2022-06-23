adeb1e4cbe824b0d6d39fa2020813d4fdeb58560
Lena <lenavishny3108@gmail.com>
2022-06-23 18:41:36 +0300

    initial

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+const productName = 'fridge';
+const productAmount = 25;
+const productCategory = 'hardware';
+const productPrice = 50000;
+
+//console.log(productName);
+
+const totalAmount = productAmount * productPrice;
+console.log(totalAmount);
