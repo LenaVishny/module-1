63f23a0460c2331953511a2f2d1914689e3fb704
Lena <lenavishny3108@gmail.com>
2022-07-04 19:03:37 +0300

    lesson5

--- lesson 5/Task04/script4.js
+++ lesson 5/Task04/script4.js
@@ -1,20 +1,24 @@
 'use strict';
 
-const calculate = (cartTotal = 0, numberProducts = 0, promoCode = 0) => {
+const calculate = (cartTotal = 0, numberProducts = 0, promoCode) => {
     let sum = cartTotal;
     if(numberProducts > 10){
-         sum = sum - (sum * 0.03);
-    };
+        sum -= sum * 0.03;
+    }
+
     if(sum > 30000){
         let sumOne = sum - 30000;
-        sumOne = sumOne -  sumOne * 0.15;
-        sum = sum - sumOne;
-    };
-    if(promoCode = "METHED"){
-        sum = sum - (sum * 0.1);
-    } else if (promoCode = "G3H2Z1" && sum > 2000){
-        sum = sum - 500;
-    }; 
+        sumOne -= sumOne * 0.15;
+        sum = 30000 + sumOne;
+    }
+
+    if(promoCode.toUpperCase() === "METHED"){
+        sum -= sum * 0.1;
+    }
+    
+    if (promoCode.toUpperCase() === "G3H2Z1" && sum > 2000){
+        sum -= 500;
+    } 
 
     return sum;
 }
