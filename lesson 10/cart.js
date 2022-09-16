624798aa0973fa87f6c034db37f34a54faebb780
lena <lenavishny3108@gmail.com>
2022-09-16 19:27:40 +0300

    lesson 11

--- lesson 10/cart.js
+++ lesson 10/cart.js
@@ -1,43 +1,43 @@
 'use strict';
 
 const cart = {
-    items: [],
-    count: 0,
+  items: [],
+  count: 0,
 
-    get totalPrice(){
-        return this.calculateItemPrice();
-    },
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
 
-    add(productName, productPrice, productAmount = 1) {
-        this.items.push({
-            productName,
-            productPrice,
-            productAmount,
-        });
-        this.increaseCount(productAmount);
-        
-    },
+  add(productName, productPrice, productAmount = 1) {
+    this.items.push({
+      productName,
+      productPrice,
+      productAmount,
+    });
+    this.increaseCount(productAmount);
+  },
 
-    increaseCount(number) {
-        this.count += number;
-    },
-    
-    calculateItemPrice() {
-        return this.items.reduce((sum, {productPrice, productAmount}) => sum += productPrice * productAmount, 0)
-    },
+  increaseCount(number) {
+    this.count += number;
+  },
 
-    clear(){
-        this.items = [];
-        this.count = 0;
-    },
-    print(){
-        console.log(JSON.stringify(this.items));
-        console.log(this.totalPrice());
-    },
+  calculateItemPrice() {
+    return this.items.reduce((sum, {productPrice, productAmount}) =>
+      sum += productPrice * productAmount, 0);
+  },
+
+  clear() {
+    this.items = [];
+    this.count = 0;
+  },
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(this.totalPrice());
+  },
 };
 
 cart.add('boots', 5000, 1);
 cart.add('skirt', 3000, 2);
 cart.add('sweater', 4000, 3);
-cart.totalPrice = 10;
+
 cart.print();
