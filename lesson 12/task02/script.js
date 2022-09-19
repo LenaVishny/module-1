552c3a59ebbde8ea17a50d76f1f1eedfa71ace61
lena <lenavishny3108@gmail.com>
2022-09-19 18:35:41 +0300

    lesson 12 task02

--- lesson 12/task02/script.js
+++ lesson 12/task02/script.js
@@ -1,19 +1,5 @@
 'use strict';
 
-const getRandomArray2 = a => {
-  const getRandomNumber2 = () => {
-    const randomNumber2 = Math.floor((Math.random() * 10) + 1);
-    return randomNumber2;
-  };
-  const randomArray2 = [];
-  for (let i = 0; i < a; i++) {
-    randomArray2.push(getRandomNumber2());
-  }
-  return randomArray2;
-};
-
-console.log(getRandomArray2(10));
-
 const getRandomArray = a => {
   const getRandomNumber = () => {
     const randomNumber = Math.floor((Math.random() * 10) + 1);
@@ -21,11 +7,14 @@ const getRandomArray = a => {
   };
   const randomArray = [];
   // вот эту часть ниже надо переделать? я не понимаю как?
-  if (a <= 0) {
-    return 0;
-  } else {
-    return a + randomArray.push(getRandomNumber());
+  for (let i = 0; i < a; i++) {
+    randomArray.push(getRandomNumber());
   }
-  return randomArray;
+  const result = (randomArray) => randomArray.reduce((sum, num) => sum + num, 0);
+  if (result < 50) {
+    randomArray.push(getRandomNumber());
+  } else {
+    return randomArray;
+  }
 };
 console.log(getRandomArray(10));
