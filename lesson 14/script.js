95653abae26fbeb742bc3c8ea465e3e2378ff6d3
lena <lenavishny3108@gmail.com>
2022-11-04 15:10:42 +0300

    game

--- lesson 14/script.js
+++ lesson 14/script.js
@@ -1,85 +1,45 @@
 'use strict';
 (() => {
-    const balls = [1, 2, 3, 4, 5];
-    let player = 5;
-    let bot = 5;
-    let resPlayer;
-    let resBot;
+    const game = () => {
+        const ball = {
+            player: 5,
+            bot: 5,
+        };
 
-    const randomInteger = (m, n) => {
-        const min = Math.floor(m < n ? m : n);
-        const max = Math.ceil(m > n ? m : n);
+    const randomInteger = (min, max) => {
+        max = Math.floor(max);
+        min = Math.ceil(min);
         return Math.floor(Math.random() * (max - min + 1) + min);
     };
     
-
-    const getBotA = (ball) => {
-        if (Array.isArray(ball)) {
-            const ran = randomInteger(0, ball.length - 1);
-            return ball[ran];
+    return function start()  {
+        const randomNum = randomInteger(0, 1); 
+        let getPlayer = prompt(`Введи число от 1 до ${ball.player}`, '');
+        if (randomNum === 0) {
+            console.log('Бот загадал Четное!');
+        } else {
+            console.log('Бот загадал Нечетное!');
         }
-        return;
+        if (!(getPlayer === null) && !(getPlayer === "") && ball.player > 0 && ball.bot > 0 && !isNaN(getPlayer)) {
+            if (getPlayer % 2 === randomNum) {
+                ball.player -= +getPlayer;
+                ball.bot += +getPlayer;
+                alert('Бот угадал!');
+                alert(`Счет: Бот - ${ball.bot}  Игрок - ${ball.player}`);
+            } else {
+                ball.player += +getPlayer;
+                ball.bot -= +getPlayer;
+                alert('Игрок угадал!');
+                alert(`Счет: Бот - ${ball.bot}  Игрок - ${ball.player}`);
+            }
+        }
+        if (getPlayer === null || ball.player <= 0 || ball.bot <= 0) {
+            alert(`Счет: Бот - ${ball.bot}  Игрок - ${ball.player}`);
+        } else {
+            return start();
+        }
     };
-
-    const game = () => {
-        const ball = balls;
-
-        const result = {
-            player: 0,
-            bot: 0,
-            playerWin() {
-                this.resPlayer++;
-                console.log('Игрок выиграл');
-            },
-            botWin() {
-                this.resBot++;
-                console.log('Бот выиграл');
-            },
-        };
-        
-    const start = () => {
-        
-        let getPlayer = prompt('Введи число от 1 до 5');
-        getPlayer = Number(getPlayer);
-        console.log(getPlayer);
-
-        const botAnswer = getBotA(ball);
-        console.log(botAnswer);
-    
-        if (getPlayer % 2 === 0 && botAnswer % 2 === 0) {
-            resBot = bot + getPlayer;
-            console.log('Bot:', resBot);
-            resPlayer = player - getPlayer;
-            console.log('Player:', resPlayer);
-            result.botWin();
-            return start();
-
-        } else if (getPlayer % 2 === 0 && botAnswer % 2 === 1) {
-            resPlayer = getPlayer + player;
-            console.log('Player:', resPlayer);
-            resBot = bot - getPlayer;
-            console.log('Bot:', resBot);
-            result.playerWin();
-            return start();
-
-        } else if (getPlayer % 2 === 1 && botAnswer % 2 === 1) {
-            resBot = bot + getPlayer;
-            console.log('Bot:', resBot);
-            resPlayer = player - getPlayer;
-            console.log('Player:', resPlayer);
-            result.botWin();
-            return start();
-
-        } else if (resBot <= 0 || resPlayer <= 0) {
-            console.log('Конец игры');
-            result.playerWin();
-            result.botWin();
-        }  
-    }
-    
-
-return start();
 };
 
-window.RPS = game();
+window.RPS = game;
 })();
